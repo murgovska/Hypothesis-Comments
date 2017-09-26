e88d9f1299e039e543f10cf587dd1fc75a439554
Kristina Murgovska <kmurgovska@local.theearplatform.com>
2017-09-26 16:50:33 +0200

    Comments

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,9 @@ import { UserService } from '../services/index';
 
 export class HomeComponent implements OnInit {
     currentUser: User;
+    model: any = {};
     users: User[] = [];
+    comments: any = [];
 
     constructor(private userService: UserService) {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
@@ -19,6 +21,13 @@ export class HomeComponent implements OnInit {
 
     ngOnInit() {
         this.loadAllUsers();
+    }
+
+    submitComment()
+    {
+        
+        this.comments.push({username: 'murgovska', comment: this.model.comment, time: new Date()});
+        this.model.comment = '';
     }
 
     deleteUser(id: number) {
